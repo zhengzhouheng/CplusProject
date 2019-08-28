82a1c27618b60b178855ae4ff283557004ec61ab
Zhengzhouheng <zhengzhouheng@outlook.com>
2019-08-28 23:36:14 +0800

    2019_8_28_23 add string

--- CplusProject/myString.h
+++ CplusProject/myString.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class myString:public std::string
+{
+public:
+	myString() :string() {}
+	~myString();
+
+
+private:
+
+};
+
+myString::myString()
+{
+}
+
+myString::~myString()
+{
+}
